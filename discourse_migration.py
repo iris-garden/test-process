5704640faa4a559a4f24c352bd2171e318ef46f0
Iris Rademacher <irademac@broadinstitute.org>
2024-02-02 14:19:45 -0500

    asides :(

--- discourse_migration.py
+++ discourse_migration.py
@@ -1,11 +1,11 @@
 from argparse import ArgumentParser
+from bisect import insort
 from dataclasses import dataclass
-from datetime import datetime, timedelta
-from json import dumps, loads
+from json import loads
 from html.parser import HTMLParser
 from typing import Any, Callable, Dict, List, Tuple, TypeVar
 from aiohttp import ClientSession
-from asyncio import run, create_task, gather, sleep
+from asyncio import run, gather, sleep
 
 # constants
 POST_LINK_ID = "f4706281-cc60-4ff0-a0b6-b803683cc24b"
@@ -28,7 +28,7 @@ class DiscourseTopic:
     id: int
     slug: str
     title: str
-    posts: List[DiscoursePost]
+    html: str
 
 
 # html parser
@@ -39,7 +39,11 @@ class DiscourseHTMLParser(HTMLParser):
         self.post_links = set()
         # relative file links starting with /
         self.relative_link = False
-        # link previews
+        # link previews and quotes
+        self.aside = False
+        self.aside_src = None
+        self.aside_header = False
+        self.aside_local_link = None
         self.link_preview = False
         self.link_preview_url = None
         # code blocks
@@ -54,39 +58,55 @@ class DiscourseHTMLParser(HTMLParser):
     def _ref_handler(self: "DiscourseHTMLParser", name: str) -> None:
         self.output_html += f"&{name};"
 
+    def _write_starttag(
+        self: "DiscourseHTMLParser", attrs: List[Tuple[str, str]], tag: str, suffix: str
+    ) -> None:
+        attr_str_prefix = " " if len(attrs) > 0 else ""
+        attr_str = " ".join([f'{key}="{value}"' for key, value in attrs])
+        self.output_html += f"<{tag}{attr_str_prefix}{attr_str}{suffix}>"
+
     def _starttag_handler(suffix: str = "") -> None:
         def inner(
             self: "DiscourseHTMLParser", tag: str, attrs: List[Tuple[str, str]]
         ) -> None:
             attr_dict = dict(attrs)
-            if tag == "a":
-                link = attr_dict.get("href", "")
-                if "mention" in attr_dict.get("class", ""):
-                    self.mention = True
-                elif link.startswith("/"):
-                    self.relative_link = True
-                elif "https://discuss.hail.is/t/" in link:
-                    slug = link.removeprefix("https://discuss.hail.is/t/").split("/")[0]
-                    self.post_links.add(slug)
-                    self.output_html += f'<a href="{POST_LINK_ID}/{slug}">'
-            elif (
-                tag == "aside"
-                and "onebox" in attr_dict.get("class", "")
-                and "data_onebox_source" in attr_dict
-            ):
-                self.link_preview_url = attr_dict["data_onebox_source"]
-                self.output_html += f'\n<a href="{self.link_preview_url}">'
-                self.link_preview = True
-            elif tag == "pre":
-                self.code_block_pre = True
-            elif self.code_block_pre:
-                if tag == "code":
-                    self.output_html += "\n\n```python\n"
-                    self.code_block_code = True
-            else:
-                attr_str_prefix = " " if len(attrs) > 0 else ""
-                attr_str = " ".join([f'{key}="{value}"' for key, value in attrs])
-                self.output_html += f"<{tag}{attr_str_prefix}{attr_str}{suffix}>"
+            if (not self.aside) or self.aside_header:
+                if tag == "a":
+                    link = attr_dict.get("href", "")
+                    if "mention" in attr_dict.get("class", ""):
+                        self.mention = True
+                    elif link.startswith("/"):
+                        self.relative_link = True
+                    elif "https://discuss.hail.is/t/" in link:
+                        slug = link.removeprefix("https://discuss.hail.is/t/").split("/")[0]
+                        self.post_links.add(slug)
+                        self.output_html += f'<a href="{POST_LINK_ID}/{slug}">'
+                    else:
+                        self._write_starttag(attrs, tag, suffix)
+            elif not self.aside:
+                if tag == "aside":
+                    self.aside = True
+                    onebox_src = attr_dict.get("data-onebox-src", None)
+                    if onebox_src is not None:
+                        self.aside_src = onebox_src
+                        self.output_html += f'\n<a href="{onebox_src}">'
+                elif tag == "pre":
+                    self.code_block_pre = True
+                elif self.code_block_pre:
+                    if tag == "code":
+                        self.output_html += "\n\n```python\n"
+                        self.code_block_code = True
+                elif not self.aside:
+                    self._write_starttag(attrs, tag, suffix)
+            elif self.aside and (tag == "header" or (tag == "div" and "title" in attr_dict.get("class", ""))):
+                self.aside_header = True
+                self.output_html += "\n"
+            elif self.aside_header and tag=="blockquote":
+                self.aside_header = False
+                self.aside = False
+                self._write_starttag(attrs, tag, suffix)
+            elif self.aside_header and tag=="article":
+                self.aside_header = False
 
         return inner
 
@@ -101,61 +121,130 @@ class DiscourseHTMLParser(HTMLParser):
         self.output_html += f"<!--{data}-->"
 
     def handle_data(self: "DiscourseHTMLParser", data: str) -> None:
-        if self.link_preview:
-            self.output_html += f"{self.link_preview_url}"
-        elif self.mention:
+        if self.mention:
             self.output_html += f'{data.partition("@")[2]}'
+        elif self.aside_src is not None:
+            self.output_html += self.aside_src
         else:
             self.output_html += data
 
     def handle_endtag(self: "DiscourseHTMLParser", tag: str) -> None:
-        if tag == "a":
-            if self.mention:
-                self.mention = False
-            elif self.relative_link:
-                self.relative_link = False
-        elif tag == "aside" and self.link_preview:
-            self.output_html += "</a>\n"
-            self.link_preview = False
-            self.link_preview_url = None
-        elif tag == "pre":
-            self.code_block_pre = False
-        elif self.code_block_pre:
-            if tag == "code":
-                self.output_html += "\n```\n\n"
-                self.code_block_code = False
-        else:
-            self.output_html += f"</{tag}>"
+        if (not self.aside) or self.aside_header:
+            if tag == "a":
+                if self.mention:
+                    self.mention = False
+                elif self.relative_link:
+                    self.relative_link = False
+                else:
+                    self.output_html += "</a>"
+        elif not self.aside:
+            if tag == "pre":
+                self.code_block_pre = False
+            elif self.code_block_pre:
+                if tag == "code":
+                    self.output_html += "\n```\n\n"
+                    self.code_block_code = False
+            else:
+                self.output_html += f"</{tag}>"
+        elif tag == "aside":
+            self.aside = False
+            if self.aside_src is not None:
+                self.output_html += "</a>\n"
+                self.aside_src = None
 
     def handle_pi(self: "DiscourseHTMLParser", data: str) -> None:
         self.output_html += f"<?{data}>"
 
+
 # main script
-# TODO go through the topics in order of creation, make a github issue for each one, if it has links check that the link goes to something already created, otherwise make it a dummy link and keep it in a list, and then manually go through at the end and fix the dummy links :shrug:
-# or, almost this, but dummy links while parsing, replace before uploading to GH unless not found, then print out list at end of links to edit
-# we could go through and detect cycles in the links and do something really clever as far as, if there are no cycles, we can create everything in an order that makes all the links work, but frankly i don't think that's worth the effort unless it turns out we have an absolute ton of links to things that weren't already created at the time of the original topic creation
-# TODO once we create the github issue, go back and comment and close the discourse topic
 async def main(discourse_page: int, github_token: str) -> None:
     async with ClientSession() as session:
-        pages = await run_tasks([parse_page(page, session) for page in range(discourse_page + 1)])
-        topics = await run_tasks([parse_topic(topic["id"], session) for page in pages for topic in page["topic_list"]["topics"]])
-        posts = await run_tasks([parse_post(post["id"], session) for topic in topics for post in topic["post_stream"]["posts"]])
+        pages = await run_tasks(
+            [parse_page(page, session) for page in range(discourse_page + 1)]
+        )
+        topics = await run_tasks(
+            [
+                parse_topic(topic["id"], session)
+                for page in pages
+                for topic in page["topic_list"]["topics"]
+            ]
+        )
+        posts = await run_tasks(
+            [
+                parse_post(post["id"], session)
+                for topic in topics
+                for post in topic["post_stream"]["posts"]
+            ]
+        )
+
         topic_acc = {topic["id"]: {"fields": topic, "posts": []} for topic in topics}
         for post in posts:
             topic_acc[post.topic_id]["posts"].append(post)
-        topics = [DiscourseTopic(topic["fields"]["id"], topic["fields"]["slug"], topic["fields"]["title"], topic["posts"]) for topic_id, topic in topic_acc.items()]
 
         links = {}
-        for topic in topics:
-            topic_html = ""
-            links[topic.slug] = set()
-            for post in topic.posts:
-                parser = DiscourseHTMLParser()
-                parser.feed(post.html)
-                topic_html += f"<h2>{post.username} said:</h2>\n{parser.output_html}\n\n"
-                links[topic.slug] |= parser.post_links
+        topics = []
+        for topic_id, topic in topic_acc.items():
+            if topic["fields"]["slug"] != "welcome-to-the-hail-community":
+                topic_html = ""
+                links[topic["fields"]["slug"]] = {"idx": -1, "dests": set()}
+                for post in topic["posts"]:
+                    parser = DiscourseHTMLParser()
+                    parser.feed(post.html)
+                    topic_html += (
+                        f"<h2>{post.username} said:</h2>\n{parser.output_html}\n\n"
+                    )
+                    links[topic["fields"]["slug"]]["dests"] |= parser.post_links
+                insort(
+                    topics,
+                    DiscourseTopic(
+                        topic["fields"]["id"],
+                        topic["fields"]["slug"],
+                        topic["fields"]["title"],
+                        topic_html,
+                    ),
+                    key=lambda topic: topic.id,
+                )
 
-        print(links)
+        for idx, topic in enumerate(topics):
+            links[topic.slug]["idx"] = idx
+
+        first_issue_id = (
+            await run_tasks([create_issue(topics[0], session, github_token)])
+        )[0]["id"]
+
+        for src, data in links.items():
+            if len(data["dests"]) != 0:
+                for dest in data["dests"]:
+                    dest_data = links.get(dest, None)
+                    if dest_data is None:
+                        print(
+                            f"broken link: {src}->{dest} (https://github.com/iris-garden/test-process/issues/{first_issue_id + data['idx']})"
+                        )
+                    else:
+                        topics[data["idx"]].html.replace(
+                            f"{POST_LINK_ID}/{dest}",
+                            f"https://github.com/iris-garden/test-process/issues/{first_issue_id + dest_data['idx']}",
+                        )
+
+        await run_tasks(
+            [create_issue(topic, session, github_token) for topic in topics[1:]]
+        )
+
+
+async def create_issue(
+    topic: DiscourseTopic, session: ClientSession, github_token: str
+) -> str:
+    async with session.post(
+        "https://api.github.com/repos/iris-garden/test-process/issues",
+        json={"title": topic.title, "body": topic.html, "labels": ["discourse-old"]},
+        headers={
+            "Accept": "application/vnd.github+json",
+            "Authorization": f"Bearer {github_token}",
+            "Content-Type": "application/json; charset=utf-8",
+            "X-GitHub-Api-Version": "2022-11-28",
+        },
+    ) as response:
+        return loads(await response.read())
 
 
 async def run_tasks(tasks):
@@ -164,27 +253,34 @@ async def run_tasks(tasks):
         if len(tasks) > 5:
             result += await gather(*tasks[:4])
             tasks = tasks[4:]
-            await sleep(1/2)
+            await sleep(1 / 2)
         else:
             result += await gather(*tasks)
             tasks = []
     return result
 
 
-async def parse_page(discourse_page: int, session) -> None:
-    async with session.get(f"https://discuss.hail.is/latest.json?page={discourse_page}") as page_response:
-        return await page_response.json()
+async def parse_page(discourse_page: int, session: ClientSession) -> None:
+    async with session.get(
+        f"https://discuss.hail.is/latest.json?page={discourse_page}"
+    ) as response:
+        return await response.json()
 
 
-async def parse_topic(topic_id: int, session) -> None:
-    async with session.get(f"https://discuss.hail.is/t/{topic_id}.json") as topic_response:
-        return loads(await topic_response.read())
+async def parse_topic(topic_id: int, session: ClientSession) -> None:
+    async with session.get(f"https://discuss.hail.is/t/{topic_id}.json") as response:
+        return loads(await response.read())
 
 
-async def parse_post(post_id: int, session) -> None:
-    async with session.get(f"https://discuss.hail.is/posts/{post_id}.json") as post_response:
-        post_response_json = loads(await post_response.read())
-        return DiscoursePost(post_response_json["id"], post_response_json["topic_id"], post_response_json["username"], post_response_json["cooked"])
+async def parse_post(post_id: int, session: ClientSession) -> None:
+    async with session.get(f"https://discuss.hail.is/posts/{post_id}.json") as response:
+        response_json = loads(await response.read())
+        return DiscoursePost(
+            response_json["id"],
+            response_json["topic_id"],
+            response_json["username"],
+            response_json["cooked"],
+        )
 
 
 if __name__ == "__main__":
@@ -193,34 +289,3 @@ if __name__ == "__main__":
     parser.add_argument("--github_token")
     args = parser.parse_args()
     run(main(int(args.page), args.github_token))
-
-
-# issue_url = post_json(
-#     "https://api.github.com/repos/iris-garden/test-process/issues",
-#     {"title": topic.title, "body": topic_html, "labels": ["discourse-old"]},
-#     issue['url'],
-#     headers={
-#         "Accept": "application/vnd.github+json",
-#         "Authorization": f"Bearer {github_token}",
-#         "X-GitHub-Api-Version": "2022-11-28",
-#     },
-# )
-# def post_json(
-#     url: str,
-#     data: Dict[str, Any],
-#     callback: Callback = lambda x: x,
-#     headers: Dict[str, str] = {},
-# ) -> CallbackResponse:
-#     return make_request(
-#         Request(
-#             url=url,
-#             method="POST",
-#             headers={**{"Content-Type": "application/json; charset=utf-8"}, **headers},
-#             data=dumps(data).encode(),
-#         ),
-#         callback
-#     )
-# def make_request(request: Request, callback: Callback) -> CallbackResponse:
-#     with urlopen(request) as response:
-#         output = callback(loads(response.read()))
-#     return output
